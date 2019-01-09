524309d9735d8b49ba757cd5d43d7d098d1f8e76
pimpun <pimpunthomnakan@gmail.com>
2019-01-09 13:44:54 +0700

    close #5 - add new 2 files

---
+++ InstallmentEntity.java
@@ -0,0 +1,4 @@
+packet com.se
+	public class InstallmentEntity {
+		private Long Id;	
+	}
---
+++ Sale.java
@@ -0,0 +1,6 @@
+packet com.se
+	public class Sale {
+		private String Name;
+		private Long Id;
+		private Long Price;
+	}
